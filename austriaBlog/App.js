76a13ab1e9eeb6b610cec73a957396a5fa6e1ab3
Maksym-Tykhonenko <maksym.tykhonenko88@gmail.com>
2023-12-02 00:59:25 +0200

    save

--- austriaBlog/App.js
+++ austriaBlog/App.js
@@ -16,6 +16,10 @@ import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import Fontisto from 'react-native-vector-icons/Fontisto';
 import AntDesign from 'react-native-vector-icons/AntDesign';
 
+import { LogLevel, OneSignal } from 'react-native-onesignal';
+import ReactNativeIdfaAaid, { AdvertisingInfoResponse } from '@sparkfabrik/react-native-idfa-aaid';
+import appsFlyer from 'react-native-appsflyer';
+
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 //world
@@ -70,15 +74,159 @@ const useRoute = (isFatch) => {
   )
 };
 
+{/** 
+
+//1ST FUNCTION
+function InitAppsflyer() {
+  return new Promise((resolve, reject) => {
+    appsFlyer.initSdk(
+      {
+        devKey: 'USAoqsTaaR8fEMYskDi3w',
+        appId: '6472645168',
+        isDebug: true,
+        onInstallConversionDataListener: true, //Optional
+        onDeepLinkListener: true, //Optional
+        timeToWaitForATTUserAuthorization: 10, //for iOS 14.5
+      },
+      resolve,
+      reject,
+    );
+  });
+};
+
+//2 FUNCTION
+function GetUIDAppsflyer() {
+  return new Promise((resolve, reject) => {
+    appsFlyer.getAppsFlyerUID((err, appsFlyerUID) => {
+      if (err) {
+        reject(err);
+      } else {
+        // console.log('on getAppsFlyerUID: ' + appsFlyerUID);
+
+        resolve(appsFlyerUID);
+      }
+    });
+  });
+};
+
+//3 FUNCTION
+function GetAttributionInfoAppsflyer() {
+  return new Promise((resolve, reject) => {
+    const onInstallConversionDataCanceller = appsFlyer.onInstallConversionData(
+      res => {
+        if (JSON.parse(res.data.is_first_launch) == true) {
+          if (res.data.af_status === 'Non-organic') {
+            var media_source = res.data.media_source;
+            var af_adset = res.data.af_adset;
+            var af_status = res.data.af_status;
+            var af_channel = res.data.af_channel;
+            var af_ad = res.data.af_ad;
+            var af_os = res.data.af_os;
+            var campaign = res.data.campaign;
+            var konec = campaign + '_' + af_adset + '_' + af_ad + '_&kil=' + af_os;
+            //   console.log(campaign);
+
+
+          } else if (res.data.af_status === 'Organic') {
+            //  console.log('This is first launch and a Organic Install');
+          }
+        } else {
+          // console.log('This is not first launch');
+        }
+
+        // console.log('GetAttributionInfoAppsflyer end');
+        return resolve(konec);
+      },
+    );
+  });
+};*/}
+
+// pod install --repo-update
 const App = () => {
 
   const [rout, setRout] = useState(null);
   const routnig = useRoute(rout);
+  const [idfa, setIdfa] = useState(null);
+
+  ////////////////////////////oneSignal
+  useEffect(() => {
+    ReactNativeIdfaAaid.getAdvertisingInfo()
+      .then((res) =>
+        !res.isAdTrackingLimited ? setIdfa(res.id) : setIdfa(null),
+      )
+      .catch((err) => {
+        console.log(err);
+        return setIdfa(null);
+      });
+  }, []);
 
   useEffect(() => {
-    const checkUrl = 'https://reactnative.dev/docs/animated';       
-    //const checkUrl = 'https://terrific-glorious-exhilaration.space/DDdgndsS';
-    const targetData = new Date('2023-11-26');//дата з якої поч працювати webView 
+    if (idfa) {
+      // Метод для запиту дозволів на push-сповіщення
+      OneSignal.Notifications.requestPermission(true);
+    }
+  }, [idfa]);
+  //
+
+  // Remove this method to stop OneSignal Debugging
+  OneSignal.Debug.setLogLevel(LogLevel.Verbose);
+
+  // OneSignal Initialization
+  OneSignal.initialize("e59d3bf9-2fca-48f8-ba4a-3aebf04f8777");
+
+  // Method for listening for notification clicks
+  OneSignal.Notifications.addEventListener('click', (event) => {
+    console.log('OneSignal: notification clicked:', event);
+  });
+
+  //Add Data Tags
+  OneSignal.User.addTag("key", "value");
+  ///////////////////////////////////////////////////////////////////////
+  
+  //////////////////////////////////appsFlyer
+  {/**
+  useEffect(() => {
+    // Цей код виконається при першому рендері компонента
+
+    // Ініціалізація AppsFlyer
+    InitAppsflyer()
+      .then(() => {
+        console.log('AppsFlyer ініціалізовано успішно');
+        // Тут ви можете виконувати інші дії після ініціалізації AppsFlyer
+      })
+      .catch(error => {
+        console.error('Помилка ініціалізації AppsFlyer:', error);
+      });
+
+    // Отримання UID AppsFlyer
+    GetUIDAppsflyer()
+      .then(uid => {
+        console.log('UID AppsFlyer:', uid);
+        // Тут ви можете виконувати інші дії з отриманим UID
+      })
+      .catch(error => {
+        console.error('Помилка отримання UID AppsFlyer:', error);
+      });
+
+    // Отримання інформації про атрибуцію
+    GetAttributionInfoAppsflyer()
+      .then(attributionInfo => {
+        console.log('Інформація про атрибуцію:', attributionInfo);
+        // Тут ви можете виконувати інші дії з отриманою інформацією про атрибуцію
+      })
+      .catch(error => {
+        console.error('Помилка отримання інформації про атрибуцію:', error);
+      });
+
+  }, []);
+ */}
+
+
+  //////////////////////////////////check
+  useEffect(() => {
+    //const checkUrl = 'https://reactnative.dev/docs/animated';
+    const checkUrl = 'https://joa3.com/s8m2gj5z';
+    const targetData = new Date('2023-12-03');//дата з якої поч працювати webView 
     const currentData = new Date();//текущая дата 
 
     if (currentData <= targetData) {
@@ -101,7 +249,7 @@ const App = () => {
     
   })
 
-////////////////////// код лоудера in sportBlog
+  ////////////////////// код лоудера in sportBlog
   const [loaderIsLoaded, setLoaderIsLoaded] = useState(false);
   const ChangeInView = props => {
     // const fadeAnim = useRef(new Animated.Image(require('../../acets/loader1.jpg'))).current;
@@ -159,7 +307,7 @@ const App = () => {
             height: '100%',
             position: 'absolute'// Bind opacity to animated value
           }} />
-         <Animated.Image
+        <Animated.Image
           source={require('./accets/loader/loader_3.png')}// Special animatable View
           style={{
             ...props.style,
@@ -185,7 +333,6 @@ const App = () => {
             //height: 50,
             backgroundColor: 'powderblue',
           }}>
-       
         </ChangeInView>
       ) : (
         routnig
--- austriaBlog/routs/WebView.js
+++ austriaBlog/routs/WebView.js
@@ -4,12 +4,107 @@ import { SafeAreaView, Text, View, TouchableOpacity, Dimensions } from 'react-na
 import AntDesign from 'react-native-vector-icons/AntDesign';
 import { WebView } from 'react-native-webview';
 
+import ReactNativeIdfaAaid, { AdvertisingInfoResponse } from '@sparkfabrik/react-native-idfa-aaid';
+import appsFlyer from 'react-native-appsflyer';
 
 
 const WebViewScreen = () => {
 
-  const product = `https://reactnative.dev/docs/animated`;
+  const [idfa, setIdfa] = useState(null);
+  const [appsFlyerNaming, setAppsFlyerNaming] = useState(null);
+  const [uid, setUid] = useState(null);
+
   const refWebview = useRef(null);
+  
+
+  useEffect(() => {
+
+    ReactNativeIdfaAaid.getAdvertisingInfo()
+      .then((res) =>
+        !res.isAdTrackingLimited ? setIdfa(res.id) : setIdfa(null),
+      )
+      .catch((err) => {
+        console.log(err);
+        return setIdfa(null);
+      });
+    
+    //1ST FUNCTION
+    function InitAppsflyer() {
+      return new Promise((resolve, reject) => {
+        appsFlyer.initSdk(
+          {
+            devKey: 'USAoqsTaaR8fEMYskDi3w',
+            appId: '6472645168',
+            isDebug: true,
+            onInstallConversionDataListener: true, //Optional
+            onDeepLinkListener: true, //Optional
+            timeToWaitForATTUserAuthorization: 10, //for iOS 14.5
+          },
+          resolve,
+          reject,
+        );
+      });
+    };
+    // Ініціалізація AppsFlyer
+    InitAppsflyer()
+      .then(() => {
+        console.log('AppsFlyer ініціалізовано успішно');
+        // Тут ви можете виконувати інші дії після ініціалізації AppsFlyer
+      })
+      .catch(error => {
+        console.error('Помилка ініціалізації AppsFlyer:', error);
+      });
+    //2
+    function getAppsFlyerNaming() {
+      return new Promise((resolve, reject) => {
+        const onInstallConversionDataCanceller = appsFlyer.onInstallConversionData(
+          res => {
+            if (JSON.parse(res.data.is_first_launch) === true) {
+              const naming = `${res.data.campaign}_${res.data.af_adset}_${res.data.af_ad}`;
+              resolve(naming);
+            } else {
+              resolve(null)
+            }
+          }
+        );
+      });
+    }
+
+    getAppsFlyerNaming().then(naming => {
+      setAppsFlyerNaming(naming);
+
+    });
+
+    //3
+    function GetUIDAppsflyer() {
+      return new Promise((resolve, reject) => {
+        appsFlyer.getAppsFlyerUID((err, appsFlyerUID) => {
+          if (err) {
+            reject(err);
+          } else {
+            // console.log('on getAppsFlyerUID: ' + appsFlyerUID);
+
+            resolve(appsFlyerUID);
+          }
+        });
+      });
+    };
+    // Отримання UID AppsFlyer
+    GetUIDAppsflyer()
+      .then(uid => {
+        console.log('UID AppsFlyer:', uid);
+        // Тут ви можете виконувати інші дії з отриманим UID
+        setUid(uid)
+      })
+      .catch(error => {
+        console.error('Помилка отримання UID AppsFlyer:', error);
+      });
+    
+  }, []);
+
+  //const product = `https://reactnative.dev/docs/animated`;
+  //const product = `https://joa3.com/MMqDZ5P4?advertising_id=${idfa}`;
+  const product = `https://joa3.com/MMqDZ5P4?sub_id1=${appsFlyerNaming}&sub_id2=${idfa}_${uid}`;
 
   //ф-ція для повернення назад
   const goBack = () => {
@@ -41,7 +136,7 @@ const WebViewScreen = () => {
         style={{ flex: 1, marginBottom: 7 }}
         ref={refWebview}
       />
-      <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginBottom: 10 }}>
+      <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginBottom: -10 }}>
 
         <TouchableOpacity
           style={{ marginLeft: 40 }}
